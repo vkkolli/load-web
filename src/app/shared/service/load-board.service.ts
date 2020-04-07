2e58f8805ef0657b016e6bb078e1b2dcd2c3e91f
Jagan <rjagan@ceiamerica.com>
2020-04-07 13:05:52 +0530

    Updated..

--- src/app/shared/service/load-board.service.ts
+++ src/app/shared/service/load-board.service.ts
@@ -2,24 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
-import { ILoad } from '../model/load.model';
+import { LoadBoard } from '../model/load.model';
 
 
 @Injectable()
 export class LoadBoardService {
 
     private _loadBoardUrl: string = "/assets/data/load.json";
-    private loads: ILoad[];
+    private loads: LoadBoard[];
 
     constructor(private http: HttpClient ) {
 
     }
 
-    getLoads(): Observable<ILoad[]> {
+    getLoads(): Observable<LoadBoard[]> {
         if (this.loads) {
             return of(this.loads);
         }
-        return this.http.get<ILoad[]>(this._loadBoardUrl);
+        return this.http.get<LoadBoard[]>(this._loadBoardUrl);
     }
 
    
