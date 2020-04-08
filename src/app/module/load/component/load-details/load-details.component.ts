7c777c6a5ccbdae7ca7948f44c6c9b5aa1e9dc40
Amalraj Francis <famalraj@ceiamerica.com>
2020-04-08 18:03:01 +0530

    UI Updated

--- src/app/module/load/component/load-details/load-details.component.ts
+++ src/app/module/load/component/load-details/load-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { FormBuilder, Validators } from '@angular/forms';
 import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
-import {ColumnMode} from '@swimlane/ngx-datatable';
+import {ColumnMode, SelectionType} from '@swimlane/ngx-datatable';
 import {Observable} from 'rxjs';
 import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
 const searchList= ['Abc', 'Abcde', 'bcd', 'def', 'cde', 'xyz', 'qwerty', 'asdfg', 'poiuy', 'lkjhg', 'mnbv', 'jkl'];
@@ -16,17 +16,28 @@ export class LoadDetailsComponent implements OnInit {
   activeIds = [];
   ColumnMode = ColumnMode;
   columns = [
+    { headerCheckboxable: false, checkboxable: true, width: "30" },
     { name: "Name", prop: "name" },
     { name: "City / State / Zip", prop: "cityStateZip" },
     { name: "Contact Name", prop: "contact_name" },
     { name: "Contact Email", prop: "contact_email" },
     { name: "Contact Phone", prop: "phone" }
   ];
-  rows = [
-    {
-      name: "ABC", cityStateZip: "New York, NY, 12345", contact_name: "XYZ", contact_email: "xyz@abc.com", phone: "(123) 456-7890"
-    }
+  columns1 = [
+    { name: "Name", prop: "name" },
+    { name: "City / State / Zip", prop: "cityStateZip" },
+    { name: "Contact Name", prop: "contact_name" },
+    { name: "Contact Email", prop: "contact_email" },
+    { name: "Contact Phone", prop: "phone" },
+    { name: "Actions", prop: "actions" }
   ];
+  rows = [
+    { name: "ABC", cityStateZip: "New York, NY, 12345", contact_name: "XYZ", contact_email: "xyz@abc.com", phone: "(123) 456-7890", actions: "remove" },
+    { name: "ABC", cityStateZip: "New York, NY, 12345", contact_name: "XYZ", contact_email: "xyz@abc.com", phone: "(123) 456-7890", actions: "remove" },
+    { name: "ABC", cityStateZip: "New York, NY, 12345", contact_name: "XYZ", contact_email: "xyz@abc.com", phone: "(123) 456-7890", actions: "remove" },
+    { name: "ABC", cityStateZip: "New York, NY, 12345", contact_name: "XYZ", contact_email: "xyz@abc.com", phone: "(123) 456-7890", actions: "remove" }
+  ];
+  SelectionType = SelectionType;
   
   search = (text$: Observable<string>) =>
     text$.pipe(
@@ -46,16 +57,15 @@ export class LoadDetailsComponent implements OnInit {
     customer: this.fb.group({
       name: ['', Validators.required],
       address: ['', Validators.required],
-      contact_name: ['', Validators.required],
-      contact_email: ['', Validators.required],
-      phone: ['']
+      contact_name: [{value: '', disabled: true}, Validators.required],
+      contact_email: [{value: '', disabled: true}, Validators.required],
+      phone: [{value: '', disabled: true}]
     }),    
     equipment: this.fb.group({
       type: ['', Validators.required],
       load_size: ['', Validators.required],
       length: [''],
-      weight: [''],
-      width: ['']
+      weight: ['']
     }),    
     commodity: this.fb.group({
       name: ['', Validators.required],
@@ -66,12 +76,14 @@ export class LoadDetailsComponent implements OnInit {
       name: ['', Validators.required],
       cityStateZip: [''],
       pickup_date: [''],
+      pickup_time: [''],
       notes: ['']
     }),    
     destination: this.fb.group({
       name: ['', Validators.required],
       cityStateZip: [''],
       delivery_date: [''],
+      delivery_time: [''],
       notes: ['']
     }),    
     pricing: this.fb.group({
@@ -79,8 +91,10 @@ export class LoadDetailsComponent implements OnInit {
       max: [''],
       revenue: [''],
       rev_value: [''],
+      rev_total: [{value: '', disabled: true}],
       cost: [''],
-      cost_value: ['']
+      cost_value: [''],
+      cost_total: [{value: '', disabled: true}]
     }),
     carrier: this.fb.group({
       carrier_search: ['']
