2875726a039eb4ab0cebd22a45419b8defab5812
Jagan <rjagan@ceiamerica.com>
2020-04-08 13:21:26 +0530

    Create Load Changes

--- src/app/module/load/component/load-details/load-details.component.ts
+++ src/app/module/load/component/load-details/load-details.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { FormBuilder, Validators } from '@angular/forms';
 import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
-import {ColumnMode} from '@swimlane/ngx-datatable';
-import {Observable} from 'rxjs';
-import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
-const searchList= ['Abc', 'Abcde', 'bcd', 'def', 'cde', 'xyz', 'qwerty', 'asdfg', 'poiuy', 'lkjhg', 'mnbv', 'jkl'];
+import { ColumnMode } from '@swimlane/ngx-datatable';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { CreateLoadService } from './shared/service/create-load.service';
+import { Router } from '@angular/router';
+const searchList = ['Abc', 'Abcde', 'bcd', 'def', 'cde', 'xyz', 'qwerty', 'asdfg', 'poiuy', 'lkjhg', 'mnbv', 'jkl'];
 @Component({
   selector: "app-load-details",
   templateUrl: "./load-details.component.html",
   styleUrls: ["./load-details.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [NgbAccordionConfig]
+  providers: [NgbAccordionConfig, CreateLoadService]
 })
 export class LoadDetailsComponent implements OnInit {
   activeIds = [];
@@ -27,7 +29,7 @@ export class LoadDetailsComponent implements OnInit {
       name: "ABC", cityStateZip: "New York, NY, 12345", contact_name: "XYZ", contact_email: "xyz@abc.com", phone: "(123) 456-7890"
     }
   ];
-  
+
   search = (text$: Observable<string>) =>
     text$.pipe(
       debounceTime(200),
@@ -35,13 +37,17 @@ export class LoadDetailsComponent implements OnInit {
       map(term => term.length < 2 ? []
         : searchList.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
     );
-  constructor(private fb: FormBuilder, config: NgbAccordionConfig) {
+
+  constructor(private fb: FormBuilder, config: NgbAccordionConfig, private createLoadService: CreateLoadService, private router: Router) {
     config.type = 'dark';
   }
 
   ngOnInit(): void {
     this.activeIds = ['customer', 'equip', 'commodity', 'trip', 'pricing', 'carrier', 'load_carrier'];
   }
+
+  get formControls() { return this.loadForm.controls; }
+
   loadForm = this.fb.group({
     customer: this.fb.group({
       name: ['', Validators.required],
@@ -49,31 +55,31 @@ export class LoadDetailsComponent implements OnInit {
       contact_name: ['', Validators.required],
       contact_email: ['', Validators.required],
       phone: ['']
-    }),    
+    }),
     equipment: this.fb.group({
       type: ['', Validators.required],
       load_size: ['', Validators.required],
       length: [''],
       weight: [''],
       width: ['']
-    }),    
+    }),
     commodity: this.fb.group({
       name: ['', Validators.required],
       weight: [''],
       value: ['']
-    }),    
+    }),
     origin: this.fb.group({
       name: ['', Validators.required],
       cityStateZip: [''],
       pickup_date: [''],
       notes: ['']
-    }),    
+    }),
     destination: this.fb.group({
       name: ['', Validators.required],
       cityStateZip: [''],
       delivery_date: [''],
       notes: ['']
-    }),    
+    }),
     pricing: this.fb.group({
       target: ['', Validators.required],
       max: [''],
@@ -86,4 +92,20 @@ export class LoadDetailsComponent implements OnInit {
       carrier_search: ['']
     })
   });
+
+  onSubmit() {
+    // stop here if form is invalid
+    if (this.loadForm.invalid) {
+      alert("Form Invalid...!");
+      return;
+    }
+
+    this.createLoadService.createLoad(this.loadForm)
+      .subscribe(data => {
+        if (data["success"]) {
+          alert("Load Created Successfuly..!")
+          this.router.navigate(['../']);
+        }
+      });
+  }
 }
