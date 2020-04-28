a678ab44c4d8581e2a603cd4821046112681d9f4
Sarves <tsarveswaran@ceiamerica.com>
2020-04-28 13:33:03 +0530

    Link added to load Id and columns resized

--- src/app/module/load/container/load-board/load-board.component.ts
+++ src/app/module/load/container/load-board/load-board.component.ts
@@ -9,21 +9,8 @@ import { LoadBoard } from '@app/shared/model/load.model';
 })
 export class LoadBoardComponent implements OnInit {
   loads: LoadBoard[];
-  columns = [
-    { name: "LoadId", prop: "loadId", width:'40'},
-    { name: "Age", prop: "age", width:'60'},
-    { name: "Truck", prop: "equipmentName", width:'100'},
-    { name: "F/P", prop: "loadType",width:'30' },
-    { name: "Status", prop: "loadStatus",width:'100' },
-    { name: "Origin", prop: "originCsz",width:'100' },
-    { name: "Trip", prop: "mileage",width:'30' },
-    { name: "Destination", prop: "destinationCsz",width:'100' },
-    { name: "Company", prop: "customerName" ,width:'100'},
-    { name: "Cost", prop: "revenueCost" ,width:'60'},
-    { name: "Actual Pickup", prop: "actualPickupDate" ,width:'70'},
-    { name: "Actual Delivery", prop: "actualDeliveryDate",width:'70' }
-  ];
-
+  columns = [];
+  editing = {};
 
   errorMessage: string;
   public quickSearchState: boolean = false;
