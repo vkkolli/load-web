c4f979723583233118b6960f340b0c80013518e7
Desingraj <rdesingraj@ceiamerica.com>
2020-05-08 15:17:49 +0530

    Mileage and load size

--- src/app/module/load/component/load-details/equipment/equipment.component.ts
+++ src/app/module/load/component/load-details/equipment/equipment.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
-import {ColumnMode, SelectionType} from '@swimlane/ngx-datatable';
+import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
 import { Observable } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
-import { Router } from '@angular/router';
 const searchList = ['Abc', 'Abcde', 'bcd', 'def', 'cde', 'xyz', 'qwerty', 'asdfg', 'poiuy', 'lkjhg', 'mnbv', 'jkl'];
 
 @Component({
@@ -16,11 +16,11 @@ export class EquipmentComponent implements OnInit {
   @Input() loadForm : FormGroup;
   activeIds = [];
   ColumnMode = ColumnMode;
-  
+
   SelectionType = SelectionType;
 
   public radioGroupForm: FormGroup;
-  
+
   search = (text$: Observable<string>) =>
     text$.pipe(
       debounceTime(200),
@@ -34,15 +34,12 @@ export class EquipmentComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.radioGroupForm = this.fb.group({
-      'model': 1
-    });
   }
 
   get formControls() { return this.equipForm.controls; }
 
   equipForm = this.fb.group({
-    
+
     equipment: this.fb.group({
       type: ['', Validators.required],
       load_size: ['', Validators.required],
